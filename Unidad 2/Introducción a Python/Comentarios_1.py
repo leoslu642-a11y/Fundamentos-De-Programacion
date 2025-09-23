efe04ab89e202c28fcc6152bb863088084daca3f
Fatima Gonzalez <leoslu642@gmail.com>
2025-09-23 07:45:15 -0600

    Comentarios_1.py

---
+++ Unidad 2/Introducción a Python/Comentarios_1.py
@@ -0,0 +1,12 @@
+#Este programa imprime la variable. Tambien observamos la mutabilidad de python
+
+entero = 10
+flotante = 3.14
+ cadena = "Ed Flores"
+booleana = True
+
+print(entero)
+print(flotante)
+
+Ed Flores
+ITPA
